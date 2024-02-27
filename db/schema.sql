c4e0ad14fc75ba4eefc7590f67c1d2efc5f17088
elroy <devinjl665@gmail.com>
2024-02-27 14:40:22 -0600

    Added schema code

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE departments (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES departments(id)
+);
+
+CREATE TABLE employees (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,28 @@
+INSERT INTO departments (name)
+VALUES ('Sales'),
+       ('Management'),
+       ('IT'),
+       ('Customer Service'),
+       ('Payment Processing');
+
+
+
+INSERT INTO positions (id, title, salary, department_id)
+VALUES (10, 'Salesperson', 60000, 1),
+       (21,'Team Lead', 65000, 2),
+       (32, 'Front End Developer', 80000, 3),
+       (43, 'Customer Service Representative', 52000, 4),
+       (54, 'Payment Processing Representative', 42800, 5),
+       (65, 'Back End Developer', 81000, 3),
+       (76, 'Pre-Customer Service Resprentative', 40000, 4);
+
+
+
+INSERT INTO employees (id, last_name, first_name, position_id, manager_id)
+VALUES (1, 'Keaton', 'Michael',21, NULL),
+       (2, 'Tobin', 'Beef',54, 1),
+       (3, 'Winn', 'Anita Max',21, NULL),
+       (4, 'Goodman', 'Saul',10, 3),
+       (5, 'Guzman', 'Joaquin',10, 3),
+       (6, 'Pickles', 'Tommy',43, 2),
+       (7, 'Dragunov', 'Sergei',76, NULL); 
